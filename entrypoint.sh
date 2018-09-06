75c265f5a5c167fc021d248b9b6f22d408797ea4
DACRepair <DACRepair@gmail.com>
2018-09-06 16:46:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,4 @@
 
 RUN_SH="/usr/share/apacheds/bin/apacheds.sh"
 
-if [ ! -d "/usr/share/apacheds/instances/$INSTANCE" ]; then
-  mkdir "/usr/share/apacheds/instances/$INSTANCE"
-fi
-if [ ! -d "/usr/share/apacheds/instances/$INSTANCE/config" ]; then
-  cp -r /usr/share/apacheds/instances/default/* /usr/share/apacheds/instances/$INSTANCE
-fi
-
 eval "$RUN_SH $INSTANCE $@"
